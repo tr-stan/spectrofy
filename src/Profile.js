d65c67796e0f085e328391c307de9c03b551ce6d
Tristan Bennett <betriistan@gmail.com>
2019-04-24 15:53:47 -0500

    added explanation of animations

--- src/Profile.js
+++ src/Profile.js
@@ -20,7 +20,7 @@ class Profile extends Component {
             if (trackData.status !== 401) {
                 const trackDataJson = await trackData.json()
                 this.setState({
-                	trackData: trackDataJson
+                    trackData: trackDataJson
                 })
                 return trackDataJson
             } else {
@@ -37,9 +37,13 @@ class Profile extends Component {
     render() {
         return (
             <div>
-				<Search getTrackData={this.getTrackData} trackData={this.state.trackData} accessToken={this.props.accessToken}/>
-				<p id="logged">Logged in as {this.props.userInfo.display_name}</p>
-			</div>
+                <Search getTrackData={this.getTrackData} trackData={this.state.trackData} accessToken={this.props.accessToken}/>
+                <p>Each bubble in a track's visualization represents a 0.2 - 1 second-long segment of the track provided by Spotify's 'track audio analysis' API endpoint.</p>
+                <p>Each bubble's diameter/size is determined by the maximum loudness of that specific segment.</p>
+                <p>Each bubble's color is determined by it's pitch data. I converted the pitch data for the segment into an RGBA value, with the opacity determined by the data confidence for that specific segment of data.</p>
+                <p>Lastly, I really wanted to incorporate the timbre data and some randomness into the visualization. So, I took a random data point from each segment's array of timbre data and used it to determine the bubble's starting location and velocity.</p>     
+                <p id="logged">Logged in as {this.props.userInfo.display_name}</p>
+            </div>
         )
     }
 }
